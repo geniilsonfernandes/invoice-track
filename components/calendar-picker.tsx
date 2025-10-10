631006772302d85bd14be84a91c2c56ae01cf957
Gee <geniilsonfernandes@gmail.com>
2025-10-10 15:40:13 -0300

    feat(calendar): refactor CalendarPicker to use Drawer for mobile and enhance input handling

--- components/calendar-picker.tsx
+++ components/calendar-picker.tsx
@@ -1,12 +1,24 @@
 "use client";
 
-import { parseDate } from "chrono-node";
 import * as React from "react";
 
 import { Calendar } from "components/ui/calendar";
-import { Label } from "components/ui/label";
 import { Popover, PopoverContent, PopoverTrigger } from "components/ui/popover";
+import { useIsDesktop } from "hooks/useIsDesktop";
 import { CalendarCheck } from "lucide-react";
+import { Matcher } from "react-day-picker";
+import { ControllerFieldState } from "react-hook-form";
+import { Button } from "./ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "./ui/drawer";
+import { Field, FieldError, FieldLabel } from "./ui/field";
 import { InputGroup, InputGroupAddon, InputGroupInput } from "./ui/input-group";
 
 function formatDate(date: Date | undefined) {
@@ -23,81 +35,116 @@ function formatDate(date: Date | undefined) {
 
 type CalendarPickerProps = {
   id?: string;
-  name?: string;
   placeholder?: string;
-  onChange?: (value: string) => void;
+  onChange?: (value: Date | undefined) => void;
   date?: Date;
   label?: string;
+  fieldState?: ControllerFieldState;
+  disabled?: Matcher | Matcher[] | undefined;
 };
 
 export function CalendarPicker({
   placeholder = "Select a date",
   id,
-  name,
-  label,
+  label = "Select a date",
   onChange,
   date,
+  fieldState,
+  disabled,
 }: CalendarPickerProps) {
-  const [open, setOpen] = React.useState(false);
-  const [value, setValue] = React.useState("In 2 days");
-  const [dateSelected, setDateSelected] = React.useState<Date | undefined>(
-    parseDate(value) || undefined
-  );
-  const [month, setMonth] = React.useState<Date | undefined>(date);
+  const isDesktop = useIsDesktop();
 
- 
-  return (
-    <div className="w-full flex flex-1 flex-col gap-3 leading-snug">
-      <Label htmlFor={id} className="block text-sm">
-        {label || "Due Date"}
-      </Label>
-      <div className="w-full">
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState(date);
+  const [dateSelected, setDateSelected] = React.useState<Date | undefined>(
+    value
+  );
+
+  const renderInput = () => {
+    return (
+      <InputGroup>
+        <InputGroupInput
+          id={id}
+          type="text"
+          value={value?.toDateString() || ""}
+          placeholder={placeholder}
+          readOnly
+          className="w-full text-left "
+          onChange={(e) => {
+            setValue(new Date(e.target.value));
+          }}
+          onKeyDown={(e) => {
+            if (e.key === "ArrowDown") {
+              e.preventDefault();
+              setOpen(true);
+            }
+
+            if (e.key === "Enter") {
+              e.preventDefault();
+              setOpen(true);
+            }
+          }}
+        />
+        <InputGroupAddon align="inline-end">
+          <CalendarCheck />
+        </InputGroupAddon>
+      </InputGroup>
+    );
+  };
+
+  const renderCalendar = () => {
+    return (
+      <Calendar
+        mode="single"
+        className="mx-auto"
+        selected={dateSelected}
+        disabled={disabled}
+        onSelect={(value) => {
+          setValue(value);
+          setOpen(false);
+          setDateSelected(value);
+          onChange?.(value);
+        }}
+      />
+    );
+  };
+
+  if (isDesktop) {
+    return (
+      <Field>
+        <FieldLabel htmlFor={id}> {label}</FieldLabel>
         <Popover open={open} onOpenChange={setOpen}>
           <PopoverTrigger className="w-full" asChild>
-            <InputGroup>
-              <InputGroupInput
-                id={id}
-                type="text"
-                value={value}
-                placeholder={placeholder}
-                readOnly
-                className="w-full text-left "
-                onChange={(e) => {
-                  setValue(e.target.value);
-                  const date = parseDate(e.target.value);
-                  if (date) {
-                    setDateSelected(date);
-                    setMonth(date);
-                  }
-                }}
-                onKeyDown={(e) => {
-                  if (e.key === "ArrowDown") {
-                    e.preventDefault();
-                    setOpen(true);
-                  }
-                }}
-              />
-              <InputGroupAddon align="inline-end">
-                <CalendarCheck />
-              </InputGroupAddon>
-            </InputGroup>
+            {renderInput()}
           </PopoverTrigger>
-          <PopoverContent className="w-auto overflow-hidden p-0" align="end">
-            <Calendar
-              mode="single"
-              selected={dateSelected}
-              captionLayout="dropdown"
-              month={month}
-              onMonthChange={setMonth}
-              onSelect={(date) => {
-                setDateSelected(date);
-                setValue(formatDate(date));
-                setOpen(false);
-              }}
-            />
+          <PopoverContent className="w-auto overflow-hidden" align="end">
+            {renderCalendar()}
           </PopoverContent>
         </Popover>
-      </div>
-    </div>
+        <FieldError>{fieldState?.error?.message}</FieldError>
+      </Field>
+    );
+  }
+
+  return (
+    <Field>
+      <FieldLabel htmlFor={id}> {label}</FieldLabel>
+      <Drawer open={open} onOpenChange={setOpen} fixed>
+        <DrawerTrigger asChild>{renderInput()}</DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader className="text-left">
+            <DrawerTitle>Select a date</DrawerTitle>
+          </DrawerHeader>
+          {renderCalendar()}
+          <DrawerFooter className="pt-2 flex">
+            <DrawerClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DrawerClose>
+            <Button>Save changes</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+      <FieldError>{fieldState?.error?.message}</FieldError>
+    </Field>
   );
 }
