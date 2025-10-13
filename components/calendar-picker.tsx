9c8a8bdac29e4716046be3ff9fcc86a22fdcddee
Gee <geniilsonfernandes@gmail.com>
2025-10-13 14:26:40 -0300

    feat(calendar-picker): refactor CalendarPicker component to improve input handling and UI structure

--- components/calendar-picker.tsx
+++ components/calendar-picker.tsx
@@ -1,14 +1,7 @@
 "use client";
 
-import * as React from "react";
-
+import { Button } from "components/ui/button";
 import { Calendar } from "components/ui/calendar";
-import { Popover, PopoverContent, PopoverTrigger } from "components/ui/popover";
-import { useIsDesktop } from "hooks/useIsDesktop";
-import { CalendarCheck } from "lucide-react";
-import { Matcher } from "react-day-picker";
-import { ControllerFieldState } from "react-hook-form";
-import { Button } from "./ui/button";
 import {
   Drawer,
   DrawerClose,
@@ -17,133 +10,112 @@ import {
   DrawerHeader,
   DrawerTitle,
   DrawerTrigger,
-} from "./ui/drawer";
-import { Field, FieldError, FieldLabel } from "./ui/field";
-import { InputGroup, InputGroupAddon, InputGroupInput } from "./ui/input-group";
-
-function formatDate(date: Date | undefined) {
-  if (!date) {
-    return "";
-  }
-
-  return date.toLocaleDateString("en-US", {
-    day: "2-digit",
-    month: "long",
-    year: "numeric",
-  });
-}
+} from "components/ui/drawer";
+import { Field, FieldError, FieldLabel } from "components/ui/field";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "components/ui/input-group";
+import { Popover, PopoverContent, PopoverTrigger } from "components/ui/popover";
+import { useIsDesktop } from "hooks/useIsDesktop";
+import { CalendarCheck } from "lucide-react";
+import * as React from "react";
+import { Matcher } from "react-day-picker";
+import { ControllerFieldState } from "react-hook-form";
 
 type CalendarPickerProps = {
   id?: string;
   placeholder?: string;
-  onChange?: (value: Date | undefined) => void;
-  date?: Date;
   label?: string;
+  date?: Date;
+  onChange?: (value: Date | undefined) => void;
   fieldState?: ControllerFieldState;
-  disabled?: Matcher | Matcher[] | undefined;
-};
+  disabled?: Matcher | Matcher[];
+} & React.HTMLAttributes<HTMLDivElement>;
 
 export function CalendarPicker({
-  placeholder = "Select a date",
   id,
   label = "Select a date",
-  onChange,
+  placeholder = "Select a date",
   date,
+  onChange,
   fieldState,
   disabled,
+  ...props
 }: CalendarPickerProps) {
   const isDesktop = useIsDesktop();
-
   const [open, setOpen] = React.useState(false);
-  const [value, setValue] = React.useState(date);
-  const [dateSelected, setDateSelected] = React.useState<Date | undefined>(
-    value
-  );
+  const [month, setMonth] = React.useState(date);
 
-  const renderInput = () => {
-    return (
-      <InputGroup>
-        <InputGroupInput
-          id={id}
-          type="text"
-          value={value?.toDateString() || ""}
-          placeholder={placeholder}
-          readOnly
-          className="w-full text-left "
-          onChange={(e) => {
-            setValue(new Date(e.target.value));
-          }}
-          onKeyDown={(e) => {
-            if (e.key === "ArrowDown") {
-              e.preventDefault();
-              setOpen(true);
-            }
-
-            if (e.key === "Enter") {
-              e.preventDefault();
-              setOpen(true);
-            }
-          }}
-        />
-        <InputGroupAddon align="inline-end">
-          <CalendarCheck />
-        </InputGroupAddon>
-      </InputGroup>
-    );
+  const handleSelect = (value: Date | undefined) => {
+    setMonth(value);
+    onChange?.(value);
+    setOpen(false);
   };
 
-  const renderCalendar = () => {
-    return (
-      <Calendar
-        mode="single"
-        className="mx-auto"
-        selected={dateSelected}
-        disabled={disabled}
-        onSelect={(value) => {
-          setValue(value);
-          setOpen(false);
-          setDateSelected(value);
-          onChange?.(value);
+  const renderInput = (
+    <InputGroup>
+      <InputGroupInput
+        id={id}
+        type="text"
+        value={date ? date.toLocaleDateString() : ""}
+        placeholder={placeholder}
+        readOnly
+        className="w-full text-left cursor-pointer"
+        onKeyDown={(e) => {
+          if (["ArrowDown", "Enter"].includes(e.key)) {
+            e.preventDefault();
+            setOpen(true);
+          }
         }}
       />
-    );
-  };
+      <InputGroupAddon align="inline-end">
+        <CalendarCheck />
+      </InputGroupAddon>
+    </InputGroup>
+  );
 
-  if (isDesktop) {
-    return (
-      <Field>
-        <FieldLabel htmlFor={id}> {label}</FieldLabel>
-        <Popover open={open} onOpenChange={setOpen}>
-          <PopoverTrigger className="w-full" asChild>
-            {renderInput()}
-          </PopoverTrigger>
-          <PopoverContent className="w-auto overflow-hidden" align="end">
-            {renderCalendar()}
-          </PopoverContent>
-        </Popover>
-        <FieldError>{fieldState?.error?.message}</FieldError>
-      </Field>
-    );
-  }
+  const renderCalendar = (
+    <Calendar
+      mode="single"
+      className="mx-auto"
+      month={month}
+      onMonthChange={setMonth}
+      selected={date}
+      disabled={disabled}
+      onSelect={handleSelect}
+    />
+  );
 
   return (
-    <Field>
-      <FieldLabel htmlFor={id}> {label}</FieldLabel>
-      <Drawer open={open} onOpenChange={setOpen} fixed>
-        <DrawerTrigger asChild>{renderInput()}</DrawerTrigger>
-        <DrawerContent>
-          <DrawerHeader className="text-left">
-            <DrawerTitle>Select a date</DrawerTitle>
-          </DrawerHeader>
-          {renderCalendar()}
-          <DrawerFooter className="pt-2 flex">
-            <DrawerClose asChild>
-              <Button variant="outline">Cancel</Button>
-            </DrawerClose>
-            <Button>Save changes</Button>
-          </DrawerFooter>
-        </DrawerContent>
-      </Drawer>
+    <Field {...props}>
+      <FieldLabel htmlFor={id}>{label}</FieldLabel>
+
+      {isDesktop ? (
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger asChild>{renderInput}</PopoverTrigger>
+          <PopoverContent className="w-auto overflow-hidden" align="end">
+            {renderCalendar}
+          </PopoverContent>
+        </Popover>
+      ) : (
+        <Drawer open={open} onOpenChange={setOpen} fixed>
+          <DrawerTrigger asChild>{renderInput}</DrawerTrigger>
+          <DrawerContent>
+            <DrawerHeader className="text-left">
+              <DrawerTitle>{label}</DrawerTitle>
+            </DrawerHeader>
+            {renderCalendar}
+            <DrawerFooter className="pt-2 flex">
+              <DrawerClose asChild>
+                <Button variant="outline">Cancel</Button>
+              </DrawerClose>
+            </DrawerFooter>
+          </DrawerContent>
+        </Drawer>
+      )}
+
       <FieldError>{fieldState?.error?.message}</FieldError>
     </Field>
   );
